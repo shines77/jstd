e20cd51612ae9ca5e743bcfaaefa08a2743ac824
GuoXH <gz_shines@msn.com>
2022-06-21 00:32:41 +0800

    更新 /jstd/system/RandomGen.h 等
    Signed-off-by: GuoXH <gz_shines@msn.com>

--- src/main/jstd/system/RandomGen.h
+++ src/main/jstd/system/RandomGen.h
@@ -1,4 +1,52 @@
 
+/************************************************************************************
+
+  CC BY-SA 4.0 License
+
+  Copyright (c) 2017-2022 XiongHui Guo (gz_shines@msn.com)
+
+  https://github.com/shines77/jstd_hash_map
+  https://gitee.com/shines77/jstd_hash_map
+
+*************************************************************************************
+
+  CC Attribution-ShareAlike 4.0 International
+
+  https://creativecommons.org/licenses/by-sa/4.0/deed.en
+
+  You are free to:
+
+    1. Share -- copy and redistribute the material in any medium or format.
+
+    2. Adapt -- remix, transforn, and build upon the material for any purpose,
+    even commerically.
+
+    The licensor cannot revoke these freedoms as long as you follow the license terms.
+
+  Under the following terms:
+
+    * Attribution -- You must give appropriate credit, provide a link to the license,
+    and indicate if changes were made. You may do so in any reasonable manner,
+    but not in any way that suggests the licensor endorses you or your use.
+
+    * ShareAlike -- If you remix, transform, or build upon the material, you must
+    distribute your contributions under the same license as the original.
+
+    * No additional restrictions -- You may not apply legal terms or technological
+    measures that legally restrict others from doing anything the license permits.
+
+  Notices:
+
+    * You do not have to comply with the license for elements of the material
+    in the public domain or where your use is permitted by an applicable exception
+    or limitation.
+
+    * No warranties are given. The license may not give you all of the permissions
+    necessary for your intended use. For example, other rights such as publicity,
+    privacy, or moral rights may limit how you use the material.
+
+************************************************************************************/
+
 #ifndef JSTD_SYSTEM_RANDOMGEN_H
 #define JSTD_SYSTEM_RANDOMGEN_H
 
@@ -6,19 +54,10 @@
 #pragma once
 #endif
 
-#include "jstd/basic/stddef.h"
-#include "jstd/basic/stdint.h"
-#include "jstd/basic/stdsize.h"
-
-#include <stdlib.h>     // For ::srand(), ::rand()
-#include <time.h>
-
-#include <cstdint>
-#include <cstddef>
-#include <cstdlib>      // For std::srand(), std::rand()
+#include <assert.h>
 
 #include "jstd/system/LibcRandom.h"
-#include "jstd/system/MT19937.h"
+#include "jstd/system/MT19937_32.h"
 #include "jstd/system/MT19937_64.h"
 
 namespace jstd {
@@ -29,9 +68,7 @@ public:
     typedef BasicRandomGenerator<RandomAlgorithm>   this_type;
     typedef RandomAlgorithm                         random_algorithm_t;
     typedef typename RandomAlgorithm::value_type    value_type;
-
-private:
-    static random_algorithm_t random_;
+    typedef typename RandomAlgorithm::size_type     size_type;
 
 public:
     explicit BasicRandomGenerator(value_type initSeed = 0) {
@@ -40,47 +77,49 @@ public:
 
     ~BasicRandomGenerator() {}
 
+    static random_algorithm_t & getInstance() {
+        static random_algorithm_t random_;
+        return random_;
+    }
+
     static value_type rand_max() {
-        return this_type::random_.rand_max();
+        return this_type::getInstance().rand_max();
     }
 
     static void srand(value_type initSeed = 0) {
-        this_type::random_.srand(initSeed);
+        this_type::getInstance().srand(initSeed);
     }
 
     static value_type rand() {
-        return this_type::random_.rand();
+        return this_type::getInstance().rand();
     }
 
     static std::int32_t nextInt32() {
-        return this_type::random_.nextInt32();
+        return this_type::getInstance().nextInt32();
     }
 
     static std::uint32_t nextUInt32() {
-        return this_type::random_.nextUInt32();
+        return this_type::getInstance().nextUInt32();
     }
 
     static std::int64_t nextInt64() {
-        return this_type::random_.nextInt64();
+        return this_type::getInstance().nextInt64();
     }
 
     static std::uint64_t nextUInt64() {
-        return this_type::random_.nextUInt64();
+        return this_type::getInstance().nextUInt64();
+    }
+
+    static std::size_t nextInt() {
+        return this_type::getInstance().nextInt();
+    }
+
+    static std::size_t nextUInt() {
+        return this_type::getInstance().nextUInt();
     }
 
     static std::int32_t nextInt32(std::int32_t minValue, std::int32_t maxValue) {
-        std::int32_t result;
-        if (minValue < maxValue) {
-            result = minValue + std::int32_t(this_type::nextUInt32() %
-                                            std::uint32_t(maxValue - minValue + 1));
-        }
-        else if (minValue > maxValue) {
-            result = maxValue + std::int32_t(this_type::nextUInt32() %
-                                            std::uint32_t(minValue - maxValue + 1));
-        }
-        else {
-            result = minValue;
-        }
+        std::int32_t result = this_type::nextInteger<std::int32_t, std::uint32_t>(minValue, maxValue);
         return result;
     }
 
@@ -89,18 +128,7 @@ public:
     }
 
     static std::uint32_t nextUInt32(std::uint32_t minValue, std::uint32_t maxValue) {
-        std::uint32_t result;
-        if (minValue < maxValue) {
-            result = minValue + (this_type::nextUInt32() %
-                                std::uint32_t(maxValue - minValue + 1));
-        }
-        else if (minValue > maxValue) {
-            result = maxValue + (this_type::nextUInt32() %
-                                std::uint32_t(minValue - maxValue + 1));
-        }
-        else {
-            result = minValue;
-        }
+        std::uint32_t result = this_type::nextInteger<std::uint32_t>(minValue, maxValue);
         return result;
     }
 
@@ -109,18 +137,7 @@ public:
     }
 
     static std::int64_t nextInt64(std::int64_t minValue, std::int64_t maxValue) {
-        std::int64_t result;
-        if (minValue < maxValue) {
-            result = minValue + std::int64_t(this_type::nextUInt64() %
-                                            std::uint64_t(maxValue - minValue + 1));
-        }
-        else if (minValue > maxValue) {
-            result = maxValue + std::int64_t(this_type::nextUInt64() %
-                                            std::uint64_t(minValue - maxValue + 1));
-        }
-        else {
-            result = minValue;
-        }
+        std::int64_t result = this_type::nextInteger<std::int64_t, std::uint64_t>(minValue, maxValue);
         return result;
     }
 
@@ -129,23 +146,30 @@ public:
     }
 
     static std::uint64_t nextUInt64(std::uint64_t minValue, std::uint64_t maxValue) {
-        std::uint64_t result;
-        if (minValue < maxValue) {
-            result = minValue + (this_type::nextUInt64() %
-                                std::uint64_t(maxValue - minValue + 1));
-        }
-        else if (minValue > maxValue) {
-            result = maxValue + (this_type::nextUInt64() %
-                                std::uint64_t(minValue - maxValue + 1));
-        }
-        else {
-            result = minValue;
-        }
+        std::uint64_t result = this_type::nextInteger<std::uint64_t>(minValue, maxValue);
         return result;
     }
 
     static std::uint64_t nextUInt64(std::uint64_t maxValue) {
         return this_type::nextUInt64(0, maxValue);
+    }
+
+    static std::intptr_t nextInt(std::intptr_t minValue, std::intptr_t maxValue) {
+        std::intptr_t result = this_type::nextInteger<std::intptr_t, std::size_t>(minValue, maxValue);
+        return result;
+    }
+
+    static std::intptr_t nextInt(std::intptr_t maxValue) {
+        return this_type::nextUInt(0, maxValue);
+    }
+
+    static std::size_t nextUInt(std::size_t minValue, std::size_t maxValue) {
+        std::size_t result = this_type::nextInteger<std::size_t>(minValue, maxValue);
+        return result;
+    }
+
+    static std::size_t nextUInt(std::size_t maxValue) {
+        return this_type::nextUInt(0, maxValue);
     }
 
     static float nextFloat() {
@@ -203,15 +227,47 @@ public:
         }
         return result;
     }
+
+private:
+    template <typename ValueType>
+    static ValueType nextInteger() {
+        if (sizeof(ValueType) == 4)
+            return static_cast<ValueType>(this_type::nextInt32());
+        else
+            return static_cast<ValueType>(this_type::nextInt64());
+    }
+
+    template <typename ValueType, typename UnsignedType = ValueType>
+    static ValueType nextInteger(ValueType minValue, ValueType maxValue) {
+        ValueType result;
+        if (minValue <= maxValue) {
+            result = minValue + (this_type::template nextInteger<ValueType>() %
+                                 UnsignedType(maxValue - minValue + 1));
+        }
+        else {
+            assert(minValue > maxValue);
+            result = maxValue + (this_type::template nextInteger<ValueType>() %
+                                 UnsignedType(minValue - maxValue + 1));
+        }
+        return result;
+    }
+
+    template <typename ValueType, typename UnsignedType = ValueType>
+    static ValueType nextInteger(ValueType maxValue) {
+        return this_type::nextInteger<ValueType, UnsignedType>(0, maxValue);
+    }
 };
 
-template <typename RandomAlgorithm>
-typename BasicRandomGenerator<RandomAlgorithm>::random_algorithm_t
-BasicRandomGenerator<RandomAlgorithm>::random_;
-
 typedef BasicRandomGenerator<LibcRandom>    RandomGen;
-typedef BasicRandomGenerator<MT19937>       MtRandomGen;
+typedef BasicRandomGenerator<MT19937_32>    Mt32RandomGen;
 typedef BasicRandomGenerator<MT19937_64>    Mt64RandomGen;
+
+#if defined(_M_X64) || defined(_M_AMD64) || defined(_M_IA64) || defined(__amd64__) || defined(__x86_64__) \
+ || defined(__aarch64__) || defined(_M_ARM64) || defined(__ARM64__) || defined(__arm64__)
+typedef BasicRandomGenerator<MT19937_64>    MtRandomGen;
+#else
+typedef BasicRandomGenerator<MT19937_32>    MtRandomGen;
+#endif
 
 } // namespace jstd
 
